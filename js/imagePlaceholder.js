dfc36b84f503ad7657ce456a3ef055e835edaf5c
misrimod <157321091+misrimod@users.noreply.github.com>
2024-11-04 15:27:09 -0500

    Add files via upload

---
+++ js/imagePlaceholder.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('img').forEach(img => {
+    console.log("runs")
+    img.onerror = function(){
+
+        this.onerror = null;
+        this.src = "../js/placeholder-image.jpg";
+        this.alt = "";
+
+
+    }
+
+});
